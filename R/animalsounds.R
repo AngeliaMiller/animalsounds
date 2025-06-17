1abdac42be9264ff0bfc80ca19a845b501ebd7eb
Angelia Miller <amiller@nefmc.org>
2025-06-17 11:35:15 -0400

    add namespace and help documentation for function; add additional arguments to function

--- R/animalsounds.R
+++ R/animalsounds.R
@@ -7,10 +7,25 @@
 ### Copyright (c) Angelia Miller, 2025
 ### email: amiller@nefmc.org
 
-# create function
-animal_sounds <- function(animal, sound){
+
+#' Print an Animal Sound
+#'
+#' @param animal string; animal name
+#' @param country string; the country the animal is from
+#' @param sound sting; sound the animal makes
+#'
+#' @return A sentence string of what sound an animal makes
+#' @export
+#'
+#'
+#' @examples
+#' animal_sounds("dog", "France", "LeWoof")
+#'
+#'
+animal_sounds <- function(animal, country, sound){
   stopifnot(is.character(animal), length(animal) == 1)
+  stopifnot(is.character(country), length(country) == 1)
   stopifnot(is.character(sound), length(sound) == 1)
-  paste0("the ", animal, "goes ", sound, "!")
+  paste0("the ", animal, " lives in ", country, " and goes ", sound, "!")
 }
 
